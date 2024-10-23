5d25b7d817ebcc405220e9fafc6af7119039484c
dgk-dev <kangmumu@gmail.com>
2024-10-23 10:23:59 +0900

    select, select_one

--- c1021/c1021_03.py
+++ c1021/c1021_03.py
@@ -1,23 +1,35 @@
 # naver 파일저장. 리솜리조트 파일저장
-
 import requests
 
+# url = "http://www.naver.com"
+# url = "https://www.resom.co.kr/resom/main/main.asp"
+# url = "https://www.coupang.com/"
+
 # url = [
-#     "https://naver.com",
-#     "https://www.resom.co.kr/resom/main/main.asp",
-#     "https://www.daum.net/"
+#   "http://www.naver.com",
+#   "https://www.resom.co.kr/resom/main/main.asp",
+#   "http://www.daum.net/"
 # ]
+# url = ["http://www.coupang.com/"]
+# headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+# for i in range(len(url)):
+#   res = requests.get(url[i],headers=headers)
+#   res.raise_for_status() # 정상코드
 
-url = ["http://www.coupang.com"]
+#   # 파일저장
+#   with open(f"c1021/{i}.html","w",encoding="utf-8") as f:
+#     f.write(res.text)
 
-headers =  {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"}
-
-for i in range(len(url)):
-    res = requests.get(url[i], headers=headers)
-    res.raise_for_status()
-    
-    with open(f"{i}.html", "w", encoding="utf-8") as f:
-        f.write(res.text)
+# print("프로그램 종료!")    
 
 
-print("프로그램을 종료합니다.")
+# 쿠팡페이지 저장
+url = "http://www.coupang.com/"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+res = requests.get(url,headers=headers)
+res.raise_for_status() # 정상코드
+print(res.text)
+
+# 파일저장
+with open(f"c1021/coupang.html","w",encoding="utf-8") as f:
+  f.write(res.text)
--- c1021/c1021_06_next_sibling.py
+++ c1021/c1021_06_next_sibling.py
@@ -2,16 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://news.naver.com/main/ranking/popularDay.naver"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"}
-res = requests.get(url, headers=headers)
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+res = requests.get(url,headers=headers)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text,"lxml")
-# soup.find("div",{"class":"rankingnews_box_wrap"})
+
+# print(soup.find("div",{"class":"rankingnews_box_wrap"}))
 newList = soup.find("div",{"class":"rankingnews_box_wrap"}).find("div",{"class":"rankingnews_box"})
 print(1,"="*20)
 print(newList)
-print(2,"="*20)
-# next: 검색태그 다음뒤에 오는 태그를 찾아줌
+print(1,"="*20)
+
+# next : 검색태그 다음뒤에 오는 태그를 찾아줌
 # next_sibling : 검색태그의 형제관계의 다음태그를 찾아줌.
-print(newList.next_sibling)
+print(newList.next_sibling.next_sibling)
+
+
+print("여러개 개수 확인 : ",len(newList))

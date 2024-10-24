cf47ba7177dd93381fc37136ef5c98bb09ead5f5
dgk-dev <kangmumu@gmail.com>
2024-10-24 09:38:31 +0900

    네이버 로그인

--- c1023/c1023_02.py
+++ c1023/c1023_02.py
@@ -1,27 +1,14 @@
+# 파일저장 - txt,csv
 import csv
-
-# 리스트 내포
-# a = [1,2,3,4,5]
-# b = [x**2 for x in a]
-# print(b)
-
 st_list = ['순위', '종목명', '검색비율', '현재가', '전일비', '등락률', '거래량', '시가', '고가', '저가', 'PER', 'ROE']
 sto_list = ['1', '삼성전자', '11.68%', '57,800', '상승100', '+0.17%', '7,670,225', '57,500', '58,200', '57,100', '14.13', '4.15']
 sto_list2 = ['2', 'SK하이닉스', '3.25%', '190,600', '상승2,800', '+1.49%', '747,655', '188,500', '191,800', '188,000', '55.54', '-15.61']
+# csv파일저장 - 리스트 바로 저장
+# csv파일로 저장시 한글인코딩 : utf-8-sig
+with open('c1023/a.csv','w',encoding='utf-8-sig',newline="") as f:
+  writer = csv.writer(f)
+  writer.writerow(st_list)
+  writer.writerow(sto_list)
+  writer.writerow(sto_list2)
 
-# csv파일로 저장시 한글인코딩: utf-8-sig
-with open('smclass/c1023/a.csv','w',encoding='utf-8-sig', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(st_list) # 리스트 저장 가능
-    writer.writerow(sto_list)
-    writer.writerow(sto_list2)
-
-
-
-# with open('smclass/c1023/list.txt','w',encoding='utf-8') as f:
-#     f.writerow(st_list)
-#     f.writerow(sto_list)
-#     f.writerow(sto_list2)
-
-
-
+print("완료")
--- c1023/c1023_08.py
+++ c1023/c1023_08.py
@@ -1,102 +1,105 @@
+# 다음 아이디, 패스워드 로그인
 from selenium import webdriver
-from selenium.webdriver.common.by import By as by
-from selenium.webdriver.common.keys import Keys as keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
-import requests, random
+import requests
 from bs4 import BeautifulSoup
-# url = 'https://www.naver.com'
-url = 'https://flight.naver.com'
+import random
+
+# 1. 네이버항공권 페이지 열기
+url = "https://flight.naver.com/"
 browser = webdriver.Chrome()
-browser.maximize_window() # 창 최대화 (전체화면)
+browser.maximize_window() # 창 최대화
 browser.get(url)
-# 출발지 선택
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[1]/button[1]/b')
-elem.click()
+
+# 2. 출발지 - 입력,김포
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[1]/button[1]/b').click()
 time.sleep(1)
-# 김포 입력
-elem = browser.find_element(by.CLASS_NAME, 'autocomplete_input__qbYlb')
-elem.send_keys('김포')
+browser.find_element(By.CLASS_NAME,"autocomplete_input__qbYlb").send_keys("김포")
 time.sleep(1)
-# 김포 공항 선택
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[8]/div[2]/section/ul/li[2]/a/b')
-elem.click()
+# 화면창이 뜨기도 전에 선택이 되면 에러
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[8]/div[2]/section/ul/li[2]/a/b').click()
+
+
+# 3. 도착지 - 입력,제주
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[1]/button[2]/b').click()
 time.sleep(1)
-# 도착지 선택
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[1]/button[2]')
-elem.click()
+browser.find_element(By.CLASS_NAME,"autocomplete_input__qbYlb").send_keys("제주")
 time.sleep(1)
-# 제주 입력
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[8]/div[1]/div/input')
-elem.send_keys('제주')
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[8]/div[2]/section/ul/li/a/b').click()
+
+# 4. 가는날 - 날짜선택 
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[2]/button[1]').click()
 time.sleep(1)
-# 제주 선택
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[8]/div[2]/section/ul/li/a/b')
-elem.click()
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[8]/div[2]/div[1]/div[2]/div/div[3]/table/tbody/tr[2]/td[4]/button/b').click()
+# 5. 오는날 - 날짜선택
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[2]/button[2]').click()
 time.sleep(1)
-# 출발 날짜
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[2]/button[1]')
-elem.click()
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[8]/div[2]/div[1]/div[2]/div/div[3]/table/tbody/tr[2]/td[7]/button/b').click()
+
+# 6. 인원 - 1명 추가
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[3]/button').click()
 time.sleep(1)
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[8]/div[2]/div[1]/div[2]/div/div[3]/table/tbody/tr[2]/td[4]/button')
-elem.click()
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[4]/div/div/div[1]/div[1]/button[2]').click()
 time.sleep(1)
-# 도착 날짜
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[2]/button[2]')
-elem.click()
+
+# 7. 항공권검색 버튼 클릭
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/button[1]').click()
 time.sleep(1)
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[8]/div[2]/div[1]/div[2]/div/div[3]/table/tbody/tr[2]/td[7]/button')
-elem.click()
+browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[2]/div/div/div[2]/button[1]').click()
 time.sleep(1)
-# 인원 선택
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[3]/button')
-elem.click()
-time.sleep(1)
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/div[4]/div/div/div[1]/div[1]/button[2]')
-elem.click()
-time.sleep(1)
-# 항공권 검색
-elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/button[1]')
-# elem = browser.find_element(by.XPATH, '//*[@id="__next"]/div/main/div[2]/div/div/div[2]/button[1]/span')
-elem.click()
-elem.click()
-# 검색중 (화면대기)
+
+
+# 8. 데이터를 검색하는 동안 대기 상태
+# 검색중 
 # time.sleep(7)
+#--------------------
+# 화면 대기 함수
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# 화면 대기
-# 화면에 찾을려고 하는 <html> 요소
-# 에러남 . 질문해야함
-# elem = WebDriverWait(browser,30).until(EC.presence_of_all_elements_located(by.XPATH, '//*[@id="__next"]/div/main/div[4]/div/div[2]'))
-time.sleep(10)
+# 화면대기 - 30초동안 대기
+# 화면에 찾을려고 하는 <html>요소가 있는지 확인
+elem = WebDriverWait(browser, 30).until(lambda x: x.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[4]/div/div[2]'))
+print(elem)
 # 화면 스크롤 내리기
+# 현재 스크롤 높이 검색 - 1000
+prev_height = browser.execute_script("return document.body.scrollHeight")
+print("최초 높이 :",prev_height)
+
+# 스크롤 내리기 - 1000
 while True:
-  # 현재 스크롤 높이 검색
-  prev_height = browser.execute_script('return document.body.scrollHeight')
-  print('최초 높이 : ', prev_height)
-  # 스크롤 내리기
-  browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+  browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
   time.sleep(2) # 다른정보가 추가될때까지 대기
-  # 높이 확인
-  curr_height = browser.execute_script('return document.body.scrollHeight')
+  # 높이 확인 - 2000
+  curr_height = browser.execute_script("return document.body.scrollHeight")
   if prev_height == curr_height:
     break
   prev_height = curr_height
-  print('현재 높이 : ', curr_height)
-    # 중지
-    # 다르면 반복
+  print("현재 높이 :",curr_height)
+
+
 # 데이터 가져와서 처리
 # BeautifulSoup 데이터 처리
-# 웹 스크래핑으로 데이터 가져오기
-soup = BeautifulSoup(browser.page_source, 'lxml')
-with open('flight.html', 'w', encoding='utf-8') as f:
+# 웹스크래핑
+soup = BeautifulSoup(browser.page_source,"lxml")
+with open('flight.html','w',encoding='utf-8') as f:
   f.write(soup.prettify())
-input('enter 키를 입력하면 프로그램이 종료합니다. ')
-browser.quit()
-# # 네이버항공권 페이지 이동
-# elem = browser.find_element(by.ID, 'query')
-# elem.send_keys('네이버 항공권')
-# elem.send_keys(keys.ENTER)
-# # 네이버항공권 선택
-# elem = browser.find_element(by.CLASS_NAME, 'link_name')
-# elem.click()
+
+input("enter키를 입력하면 프로그램이 종료됩니다.")
+browser.quit()  
+
+# 완료
 time.sleep(100)
+
+
+# # 페이지 열기
+# browser = webdriver.Chrome()
+# browser.get(url)
+# elem = browser.find_element(By.ID,"query")
+# elem.send_keys("네이버 항공권")
+# elem.send_keys(Keys.ENTER)
+# browser.find_element(By.CLASS_NAME,"link_name").click()
+
+
+
